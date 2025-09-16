1b6cbe6b6d81f6eb70f0738e2932d7945ccb1ffb
Oana <oana.stan@cea.fr>
2025-09-16 15:56:20 +0200

    add README, cmake and structure

---
+++ README.md
@@ -0,0 +1,8 @@
+# prividema-fhe
+Prividema-fhe is a cryptographic library designed to unify multiple homomorphic encryption schemes under a single framework.
+
+Requirements
+*spqlios-arithmetic
+*cmake version 3.10
+
+To compile and run
---
+++ tests/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "ggsw.h"
+
+int main() {
+    int result;
+
+    // Test addition
+    result = add(1, 10);
+    printf("1+10= %d\n", result);  // Expected output: 7
+
+    // Test subtraction
+    result = subtract(10, 5);
+    printf("10 - 5 = %d\n", result);  // Expected output: 5
+
+    return 0;
+}
