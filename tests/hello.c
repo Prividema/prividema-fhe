97bf3c2036a92e8e781a321c2532261e38e9e5af
YE Nicolas 280049 <nicolas.ye@cea.fr>
2025-10-02 16:02:23 +0200

    Fixed interface issues

--- tests/hello.c
+++ tests/hello.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#if GGSW 
+    #include "ggsw.h"
+#else
+    #include "glwe.h"
+#endif
 
 int main() {
     int result;
 
-    // Test addition
-    result = add(1, 10);
-    printf("1+10= %d\n", result);  // Expected output: 7
+    #if GGSW
+        // Test addition
+        result = add(1, 10);
+        printf("1+10= %d\n", result);  // Expected output: 7
 
-    // Test subtraction
-    result = subtract(10, 5);
-    printf("10 - 5 = %d\n", result);  // Expected output: 5
+        // Test subtraction
+        result = subtract(10, 5);
+        printf("10 - 5 = %d\n", result);  // Expected output: 5
+    #else
+        printf("GGSW Not built.\n");
+    #endif
 
     #if DEBUG
         printf("Hello Debug !\n");
