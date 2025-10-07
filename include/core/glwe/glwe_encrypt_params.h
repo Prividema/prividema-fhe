0d43f8defd97036c2ab22b88c18ba7c7eca332f2
P-EC <pierre-emmanuel.clet@cea.fr>
2025-10-07 12:01:03 +0200

    Small updates

---
+++ include/core/glwe/glwe_encrypt_params.h
@@ -0,0 +1,13 @@
+#ifndef GLWE_ENCRYPT_PARAMS_H
+#define GLWE_ENCRYPT_PARAMS_H
+
+#include <stdint.h>
+
+typedef struct glwe_encrypt_params{
+    int64_t N; // Polynomial degree
+    int64_t k; // Number of a terms
+    int64_t base; // Base used for the base-2^k representation
+    int64_t prec; // Decomposition size for base-2^k representation
+} GLWE_ENCRYPT_PARAMS;
+
+#endif // GLWE_ENCRYPT_PARAMS_H
