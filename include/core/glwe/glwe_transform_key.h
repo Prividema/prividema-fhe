4e6f1f00039ff3455acb06c3ed6c262cb8fe9b9e
Sergi Soler Arrufat <sergi@tuneinsight.com>
2026-03-18 10:43:04 +0100

    Removed lingering declaration of DFT to non-DFT key conversion function

--- include/core/glwe/glwe_transform_key.h
+++ include/core/glwe/glwe_transform_key.h
@@ -3,23 +3,6 @@
 
 #include "glwe_key.h"
 
-//! bivGLWE KEY PART (begin)
-
-/**
- * @brief Computes the input secret key out of the DFT domain.
- *
- * @param module Additionnal information for backend.
- * @param result The result bivGLWE secret key out of the DFT domain.
- * @param sk_dft A bivGLWE secret key in the DFT domain.
- *
- * @retval - `-1` if an error occurs. In this case the error is from a syscall and perror is called.
- * @retval - `0` otherwise.
- */
-int transform_glwe_secret_key_dft_to_not_dft(const MODULE* module, GLWESecretKey* result,
-                                             const GLWESecretKeyDFT* sk_dft);
-
-//! bivGLWE IN DFT SPACE PART (begin)
-
 /**
  * @brief Computes the secret key in the DFT domain.
  *
