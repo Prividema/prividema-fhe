0483b3000e8363c2d48b5c9f8f93b5845f17f166
Sergi Soler Arrufat <sergi@tuneinsight.com>
2026-03-23 11:46:14 +0100

    Improved GGSW function documentation (and public key API)

--- include/core/ggsw/ggsw.h
+++ include/core/ggsw/ggsw.h
@@ -6,7 +6,7 @@
 #include "spqlios_alias.h"
 
 /**
- * @brief Encrypts the message m into bivGGSW ciphertext res with the
+ * @brief Encrypts the message m into the bivGGSW ciphertext res according to the
  * parameters in the result object.
  *
  * @param module      Additionnal information for backend.
@@ -34,16 +34,17 @@ int ggsw_secret_encrypt(const MODULE* module, GGSWCiphertext* result, const GLWE
 int ggsw_external_product(const MODULE* module, GLWECiphertext* result, const GLWECiphertext* glwe,
                           const GGSWCiphertext* ggsw);
 
-// The three functions below are not implemented
-
-// GGSWPublicKey is a struct encapsulating everything regarding the public
-// key.
-/* Encrypts message m into GGSW ciphertext res with parameters params */
-void ggsw_public_encrypt(GGSWCiphertext* result,   // result
-                         const GLWEPublicKey* pk,  // secret key
-                         const int64_t* m,         // message
-                         const GGSWParams* params  // parameters
-);
+/**
+ * @brief Encrypts message m into GGSW ciphertext res with parameters params
+ *
+ * TODO: implement
+ *
+ * @param result The resulting GGSWCiphertext
+ * @param pk     The public key used for encryption
+ * @param m      The message to encrypt
+ *
+ * */
+int ggsw_public_encrypt(const MODULE* module, GGSWCiphertext* result, const GLWEPublicKey* pk, const PolyUniv* m_univ);
 
 //! GGSW IN DFT PART (begin)
 
@@ -52,9 +53,9 @@ void ggsw_public_encrypt(GGSWCiphertext* result,   // result
  * according to the result object
  *
  * @param module      Additionnal information for backend.
- * @param res_dft     The encrypted message.
- * @param sk_dft      The secret key.
- * @param m_univ      The message.
+ * @param res_dft     The encrypted message, in the DFT domain.
+ * @param sk_dft      The prepared secret key.
+ * @param m_univ      The message in univariate coefficient space.
  *
  * @retval • `-1` if an error occurs.
  * @retval • `0` otherwise.
