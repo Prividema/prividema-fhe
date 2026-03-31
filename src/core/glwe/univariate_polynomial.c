95ef2de66ded1e66ccbc7e778015c1f4676aece0
Sergi Soler Arrufat <sergi@tuneinsight.com>
2026-03-31 17:28:55 +0200

    Add integer torus representation TnX
    For now not used

--- src/core/glwe/univariate_polynomial.c
+++ src/core/glwe/univariate_polynomial.c
@@ -1,5 +1,8 @@
 #include "univariate_polynomial.h"
 
+#include <math.h>
+#include <stdint.h>
+
 #include "rng.h"
 
 int univ_coefs_to_dft(const MODULE* module, PolyUnivDFT* res_dft, const PolyUniv* a)
@@ -21,6 +24,8 @@ uint64_t poly_univ_bytes(const GLWEParams* params_glwe)
 
 uint64_t poly_univ_rnx_bytes(const GLWEParams* params_glwe) { return params_glwe->nn * sizeof(PolyUnivRnX); }
 
+uint64_t poly_univ_tnx_bytes(const GLWEParams* params_glwe) { return params_glwe->nn * sizeof(PolyUnivTnX); }
+
 PolyUniv* new_univ(const GLWEParams* params_glwe) { return malloc(poly_univ_bytes(params_glwe)); }
 
 void delete_univ(PolyUniv* pol) { free(pol); }
@@ -32,3 +37,25 @@ void delete_univ_rnx(PolyUnivRnX* pol) { free(pol); }
 PolyUnivDFT* new_univ_dft(const MODULE* module) { return pvda_new_vec_znx_dft(module, 1); }
 
 void delete_univ_dft(PolyUnivDFT* pol) { pvda_delete_vec_znx_dft(pol); }
+
+PolyUnivTnX* new_univ_tnx(const GLWEParams* params_glwe) { return malloc(poly_univ_tnx_bytes(params_glwe)); }
+
+void delete_univ_tnx(PolyUnivTnX* pol) { free(pol); }
+
+int univ_rnx_to_tnx(const GLWEParams* params_glwe, PolyUnivTnX* res, PolyUnivRnX* a)
+{
+	for (size_t i = 0; i < params_glwe->nn; ++i)
+	{
+		res[i] = (uint64_t)((int64_t)ldexp(a[i], 64));
+	}
+	return 1;
+}
+
+int univ_tnx_to_rnx(const GLWEParams* params_glwe, PolyUnivRnX* res, PolyUnivTnX* a)
+{
+	for (size_t i = 0; i < params_glwe->nn; ++i)
+	{
+		res[i] = ldexp((double)a[i], -64);
+	}
+	return 1;
+}
--- tests/unit/core/glwe/test_univariate_polynomial.c
+++ tests/unit/core/glwe/test_univariate_polynomial.c
@@ -62,3 +62,58 @@ Test(coef_dft_back_forth, basic)
 	delete_univ_dft(res_dft);
 	pvda_delete_module_info(module);
 }
+
+int univ_rnx_to_tnx(const GLWEParams* params_glwe, PolyUnivTnX* res, PolyUnivRnX* a)
+{
+	for (size_t i = 0; i < params_glwe->nn; ++i)
+	{
+		res[i] = (uint64_t)(ldexp(a[i] - floor(a[i]), 64));
+	}
+	return 1;
+}
+
+int univ_tnx_to_rnx(const GLWEParams* params_glwe, PolyUnivRnX* res, PolyUnivTnX* a)
+{
+	for (size_t i = 0; i < params_glwe->nn; ++i)
+	{
+		res[i] = ldexp((double)((int64_t)a[i]), -64);
+	}
+	return 1;
+}
+
+Test(tnx_rnx_encoding, known_bounded_values)
+{
+	PolyUnivRnX rnx_values[4] = {0, -0.25, 0.25, -0.5};
+	PolyUnivTnX tnx_values[4] = {0, 0xc000000000000000L, 0x4000000000000000L, 0x8000000000000000L};
+
+	PolyUnivRnX rnx_computed[4];
+	PolyUnivTnX tnx_computed[4];
+
+	GLWEParams* params_glwe = new_glwe_params(4, 1, 4, 1, 0);
+
+	univ_rnx_to_tnx(params_glwe, tnx_computed, rnx_values);
+	univ_tnx_to_rnx(params_glwe, rnx_computed, tnx_values);
+
+	for (size_t i = 0; i < sizeof(rnx_computed) / sizeof(PolyUnivRnX); ++i)
+	{
+		cr_assert(eq(u64, tnx_values[i], tnx_computed[i]));
+		cr_assert(eq(dbl, rnx_values[i], rnx_computed[i]));
+	}
+}
+
+Test(tnx_rnx_encoding, known_outbounded_values)
+{
+	PolyUnivRnX rnx_values[4] = {2, -1.25, 68.25, 0.5};
+	PolyUnivTnX tnx_values[4] = {0, 0xc000000000000000L, 0x4000000000000000L, 0x8000000000000000L};
+
+	PolyUnivTnX tnx_computed[4];
+
+	GLWEParams* params_glwe = new_glwe_params(4, 1, 4, 1, 0);
+
+	univ_rnx_to_tnx(params_glwe, tnx_computed, rnx_values);
+
+	for (size_t i = 0; i < sizeof(tnx_computed) / sizeof(PolyUnivTnX); ++i)
+	{
+		cr_assert(eq(u64, tnx_values[i], tnx_computed[i]));
+	}
+}
